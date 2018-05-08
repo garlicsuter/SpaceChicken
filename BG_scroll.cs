7baa622ae5f723385c4124fc67c574fff70cec39
garlicsuter <markasuter@gmail.com>
2018-05-08 13:38:40 -0400

    Add files via upload
    Set two identical BG sprites in the scene. Make the 2nd child of the first. Add this script to parent. 
    If offsets the second one equal to the height of the sprite so it lines up.

---
+++ BG_scroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_scroll : MonoBehaviour {
+
+	[Range(1f, 20f)]
+	public float scrollSpeed = 1f;
+	Vector2 startPos;
+	public Renderer rend;
+	private float bgHeight;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		startPos = transform.position;
+		bgHeight = rend.bounds.size.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float newPos = Mathf.Repeat (Time.time * scrollSpeed, bgHeight);
+		transform.position = startPos + Vector2.down * newPos;
+	}
+}
